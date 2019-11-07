ad2c475e3d6de1837c3e134131c38f92b951be00
donghao921 <315876821@qq.com>
2019-11-07 14:44:36 +0800

    new

--- android/AndroidFirstCode/002/Chapter03/app/src/main/java/com/imdongh/chapter03/MainActivity.java
+++ android/AndroidFirstCode/002/Chapter03/app/src/main/java/com/imdongh/chapter03/MainActivity.java
@@ -1,13 +1,42 @@
 package com.imdongh.chapter03;
 
+import android.content.DialogInterface;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+    private Button button;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        button = findViewById(R.id.bt_button1);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                AlertDialog.Builder dialog = new AlertDialog.Builder(MainActivity.this);
+                dialog.setTitle("dialog title");
+                dialog.setMessage("message");
+                dialog.setCancelable(false);
+                dialog.setNegativeButton("取消", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        // todo
+                    }
+                });
+                dialog.setPositiveButton("确定", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        // todo
+                    }
+                });
+                dialog.show();
+
+            }
+        });
     }
 }
